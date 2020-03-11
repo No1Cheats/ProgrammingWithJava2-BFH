0b450c5ce840552ed2b5b41ea944f22913c217bb
No1Cheats <jan.imhof@gmail.com>
2020-03-11 11:36:11 +0100

    Added Lists (Still ToDo)

--- src/Collections/exCollections/src/main/java/ch/bfh/App.java
+++ src/Collections/exCollections/src/main/java/ch/bfh/App.java
@@ -1,9 +1,39 @@
 package ch.bfh;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
 public final class App {
 
-    public static void main(String[] args) {
-        // TODO
+    private static ArrayList<Person> people = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader("src/Collections/exCollections/files/people.csv"));
+        String line;
+        while ((line = br.readLine()) != null) {
+            Person person = new Person(line);
+            people.add(person);
+        }
+        br.close();
+
+        //Mehrer Listne programmieren die IList implementiere
+
+
+        long startTime = System.currentTimeMillis();
+
+        String myPerson = people.get(10).getName();
+        System.out.println(myPerson);
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("Execution Time was: " + (endTime-startTime) + "ms.");
         System.out.println("done.");
     }
+
+
 }
---
+++ src/Collections/exCollections/src/main/java/ch/bfh/List1.java
@@ -0,0 +1,15 @@
+package ch.bfh;
+
+import java.util.Collection;
+
+public class List1 implements IList {
+    @Override
+    public void add(Person p) {
+
+    }
+
+    @Override
+    public Collection<Person> searchByName(String searchArg) {
+        return null;
+    }
+}
